362453ea59bb5872c8fef4c6928384b3d8e02de1
MARTINS ODIDO AREM <mcsepro@users.noreply.github.com>
2022-04-03 17:30:58 +0100

    task 1

---
+++ server.js
@@ -0,0 +1,12 @@
+let http = require('http');
+let router = require('./routes');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+};
+
+http.createServer(router.handleRequest).listen(5000); 
+ 
